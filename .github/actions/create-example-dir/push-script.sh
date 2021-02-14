88ac9d12a3ef5badd35d6ec2a3d158ebbb71bd60
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-02-14 11:04:22 +0530

    a

--- .github/actions/create-example-dir/push-script.sh
+++ .github/actions/create-example-dir/push-script.sh
@@ -7,8 +7,8 @@ git init
 git remote add origin https://github.com/learning-workflow-automation/setting-up-github-actions.git
 git fetch
 git switch -c meow
+git pull origin meow
 git branch -u upstream/meow
-git pull
 echo 'Hello' > hello.txt
 git add .
 git commit -m 'Sample upload'
