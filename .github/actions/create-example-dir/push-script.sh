4d07356c04ff610aa152717cd457fe0df17b3053
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-02-14 11:35:02 +0530

    ab

--- .github/actions/create-example-dir/push-script.sh
+++ .github/actions/create-example-dir/push-script.sh
@@ -3,10 +3,12 @@ set -e
 
 mkdir example-dir
 cd example-dir
+
 git config --global user.email "krishnagopi487.github@outlook.com"
 git config --global user.name "BOT"
+
 git init
-git remote add origin https://github.com/learning-workflow-automation/setting-up-github-actions.git
+git remote add origin https://$1@github.com/learning-workflow-automation/setting-up-github-actions.git
 git fetch
 git switch -c meow
 git pull origin meow
