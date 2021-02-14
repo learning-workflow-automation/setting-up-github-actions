c53480328195f143f3faf09b8ccc565d9be3a370
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2021-02-14 10:41:02 +0530

    Removed wrong command

--- .github/actions/create-example-dir/push-script.sh
+++ .github/actions/create-example-dir/push-script.sh
@@ -5,9 +5,11 @@ mkdir example-dir
 cd example-dir
 git init
 git remote add origin https://github.com/learning-workflow-automation/setting-up-github-actions.git
-git remote fetch
-git branch -c meow
+git fetch
+git switch -c meow
+git branch -u upstream/meow
+git pull
 echo 'Hello' > hello.txt
 git add .
 git commit -m 'Sample upload'
-git push origin meow
+git push
